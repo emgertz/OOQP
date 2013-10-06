12100f765c1ebc496672cfc4d50e22c1b0317d64
E. Michael Gertz <emgertz@mac.com>
2013-10-05 22:22:52 -0400

    Squash compiler warning
    Remove a variable set but not used.

--- src/SparseLinearAlgebra/SparseStorage.C
+++ src/SparseLinearAlgebra/SparseStorage.C
@@ -134,13 +134,12 @@ void SparseStorage::SymmetricScale( OoqpVector& scale_in )
 
 void SparseStorage::scalarMult( double num )
 {
-  int i, j, k;
+  int i, k;
 
   for ( i = 0; i < m; i++ ) {
     // Loop over all rows in the sparse matrix
     for( k = krowM[i]; k < krowM[i+1]; k++ ) {
       // Loop over the elements of the sparse row
-      j = jcolM[k];
       M[k] = M[k] * num;
     } // End loop over the elements of the sparse row
   } // End loop over all rows in the sparse matrix
